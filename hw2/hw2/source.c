0af679938125c5b237dd6785e78ed8da0228a48c
18손재민 <jasson31@naver.com>
2019-04-14 23:04:19 +0900

    Added matrix adding funtion.

--- hw2/hw2/source.c
+++ hw2/hw2/source.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <windows.h>
-#define MAX_SIZE 2
+#define MAX_SIZE 50
 #define MALLOC(p, s) \
 	if (!((p) = malloc(s))) { \
 		fprintf(stderr, "Insufficient memory"); \
@@ -97,7 +97,7 @@ void mread(int pos) {
 			node->right = hdnode[0];
 		}
 
-
+		matrixCounter++;
 		matrices[pos] = node;
 		printf("Matrix added to %d.\n", pos);
 	}
@@ -139,12 +139,190 @@ void merase(matrixPointer* node) {
 	}
 	free(*node);
 	*node = NULL;
+	matrixCounter--;
 	printf("Matrix erased.\n");
 }
-void madd() {
+void madd(matrixPointer a, matrixPointer b, int pos) {
+	if (a->u.entry.col != b->u.entry.col || a->u.entry.row != b->u.entry.row) {
+		printf("Size of two matrices are not same.\n");
+		return;
+	}
 
+	int numRows, numCols, numHeads, i;
+	int row, col, value, currentRow;
+	matrixPointer temp, last, node;
+	matrixPointer firstHead = a->right, secondHead = b->right;
+	matrixPointer firstTemp, secondTemp;
+
+	node = newNode();
+	node->tag = entry;
+	node->u.entry.row = a->u.entry.row;
+	node->u.entry.col = a->u.entry.col;
+	numCols = a->u.entry.col;
+	numRows = a->u.entry.row;
+	numHeads = (numRows > numCols) ? numRows : numCols;
+
+	for (i = 0; i < numHeads; i++) {
+		temp = newNode();
+		hdnode[i] = temp;
+		hdnode[i]->tag = head;
+		hdnode[i]->right = temp;
+		hdnode[i]->u.next = temp;
+	}
+	currentRow = 0;
+	last = hdnode[0];
+	firstTemp = firstHead->right;
+	secondTemp = secondHead->right;
+	while (firstHead != a && secondHead != b) {
+		if(firstTemp->tag == head)
+			firstTemp = firstHead->right;
+		if (secondTemp->tag == head)
+			secondTemp = secondHead->right;
+		if (firstTemp->u.entry.row < secondTemp->u.entry.row) {
+			last = hdnode[firstTemp->u.entry.row];
+			for (; firstTemp != firstHead; firstTemp = firstTemp->right) {
+				row = firstTemp->u.entry.row;
+				col = firstTemp->u.entry.col;
+				value = firstTemp->u.entry.value;
+				temp = newNode();
+				temp->tag = entry;
+				temp->u.entry.row = row;
+				temp->u.entry.col = col;
+				temp->u.entry.value = value;
+				last->right = temp;
+				last = temp;
+				hdnode[col]->u.next->down = temp;
+				hdnode[col]->u.next = temp;
+			}
+			last->right = hdnode[row];
+		}
+		else if (firstTemp->u.entry.row > secondTemp->u.entry.row) {
+			last = hdnode[secondTemp->u.entry.row];
+			for (; secondTemp != secondHead; secondTemp = secondTemp->right) {
+				row = secondTemp->u.entry.row;
+				col = secondTemp->u.entry.col;
+				value = secondTemp->u.entry.value;
+				temp = newNode();
+				temp->tag = entry;
+				temp->u.entry.row = row;
+				temp->u.entry.col = col;
+				temp->u.entry.value = value;
+				last->right = temp;
+				last = temp;
+				hdnode[col]->u.next->down = temp;
+				hdnode[col]->u.next = temp;
+			}
+			last->right = hdnode[row];
+		}
+		else if (firstTemp->u.entry.row == secondTemp->u.entry.row) {
+			last = hdnode[firstTemp->u.entry.row];
+			while (firstTemp != firstHead || secondTemp != secondHead) {
+				if (secondTemp == secondHead) {
+					col = firstTemp->u.entry.col;
+					value = firstTemp->u.entry.value;
+					firstTemp = firstTemp->right;
+				}
+				else if (firstTemp == firstHead) {
+					row = secondTemp->u.entry.row;
+					col = secondTemp->u.entry.col;
+					value = secondTemp->u.entry.value;
+					secondTemp = secondTemp->right;
+				}
+				else if (firstTemp->u.entry.col < secondTemp->u.entry.col) {
+					row = firstTemp->u.entry.row;
+					col = firstTemp->u.entry.col;
+					value = firstTemp->u.entry.value;
+					firstTemp = firstTemp->right;
+				}
+				else if (firstTemp->u.entry.col > secondTemp->u.entry.col) {
+					row = secondTemp->u.entry.row;
+					col = secondTemp->u.entry.col;
+					value = secondTemp->u.entry.value;
+					secondTemp = secondTemp->right;
+				}
+				else if (firstTemp->u.entry.col == secondTemp->u.entry.col) {
+					row = firstTemp->u.entry.row;
+					col = firstTemp->u.entry.col;
+					value = firstTemp->u.entry.value + secondTemp->u.entry.value;
+					firstTemp = firstTemp->right;
+					secondTemp = secondTemp->right;
+				}
+				if (value) {
+					temp = newNode();
+					temp->tag = entry;
+					temp->u.entry.row = row;
+					temp->u.entry.col = col;
+					temp->u.entry.value = value;
+					last->right = temp;
+					last = temp;
+					hdnode[col]->u.next->down = temp;
+					hdnode[col]->u.next = temp;
+					last->right = hdnode[row];
+				}
+			}
+		}
+		firstHead = firstHead->u.next;
+		secondHead = secondHead->u.next;
+	}
+	if (firstHead != a) {
+
+		for (i = firstHead->u.entry.row; i < node->u.entry.row; i++) {
+
+			for (firstTemp = firstHead->right; firstTemp != firstHead; firstTemp = firstTemp->right) {
+				temp = newNode();
+				temp->tag = entry;
+				temp->u.entry.row = firstTemp->u.entry.row;
+				temp->u.entry.col = firstTemp->u.entry.col;
+				temp->u.entry.value = firstTemp->u.entry.value;
+				last->right = temp;
+				last = temp;
+				hdnode[temp->u.entry.col]->u.next->down = temp;
+				hdnode[temp->u.entry.col]->u.next = temp;
+			}
+			firstHead = firstHead->u.next;
+			last->right = hdnode[row];
+			last = hdnode[row];
+		}
+	}
+	if (secondHead != b) {
+		for (i = secondHead->u.entry.row; i < node->u.entry.row; i++) {
+
+			for (secondTemp = secondHead->right; secondTemp != secondHead; secondTemp = secondTemp->right) {
+				temp = newNode();
+				temp->tag = entry;
+				temp->u.entry.row = secondTemp->u.entry.row;
+				temp->u.entry.col = secondTemp->u.entry.col;
+				temp->u.entry.value = secondTemp->u.entry.value;
+				last->right = temp;
+				last = temp;
+				hdnode[temp->u.entry.col]->u.next->down = temp;
+				hdnode[temp->u.entry.col]->u.next = temp;
+			}
+			secondHead = secondHead->u.next;
+			last->right = hdnode[row];
+			last = hdnode[row];
+		}
+	}
+
+	for (i = 0; i < numCols; i++) {
+		hdnode[i]->u.next->down = hdnode[i];
+
+		for (i = 0; i < numHeads - 1; i++)
+			hdnode[i]->u.next = hdnode[i + 1];
+		hdnode[numHeads - 1]->u.next = node;
+		node->right = hdnode[0];
+	}
+
+	matrixCounter++;
+	matrices[pos] = node;
+	printf("Result matrix added to %d.\n", pos);
 }
-void mmult() {
+void mmult(matrixPointer a, matrixPointer b, int pos) {
+	if (a->u.entry.col != b->u.entry.row) {
+		printf("Size of two matrices does not match.\n");
+		return;
+	}
+
 
 }
 void mtranspose() {
@@ -152,45 +330,66 @@ void mtranspose() {
 }
 
 int main(void) {
-	int menu;
-	int input;
+	int menu, input1, input2, input3;
+	int max, printCounter;
 	while (1) {
 		system("cls");
-		printf("%d : Read in a sparse matrix.\n", 1);
-		printf("%d : Write out a sparse matrix.\n", 2);
-		printf("%d : Erase a sparse matrix.\n", 3);
-		printf("%d : Add two sparse matrix.\n", 4);
-		printf("%d : Multiply two sparse matrix.\n", 5);
-		printf("%d : Transpose a sparse matrix.\n", 6);
-		printf("%d : Exit the program.\n", 7);
+		printf("************************************\n");
+		printf(" Existing matrices : ");
+		max = matrixCounter;
+		printCounter = 0;
+		for (int i = 0; i < max; i++) {
+			if (matrices[i] != NULL) {
+				printCounter && printCounter % 5 == 0 ? printf("\n%23d ", i) : printf("%2d ", i);
+				printCounter++;
+			}
+			else
+				max++;
+		}
+		printf("\n************************************\n\n");
+		printf("1 : Read in a sparse matrix.\n");
+		printf("2 : Write out a sparse matrix.\n");
+		printf("3 : Erase a sparse matrix.\n");
+		printf("4 : Add two sparse matrix.\n");
+		printf("5 : Multiply two sparse matrix.\n");
+		printf("6 : Transpose a sparse matrix.\n");
+		printf("7 : Exit the program.\n\n");
 		printf("Type menu : ");
 		scanf("%d", &menu);
 		system("cls");
 		switch (menu) {
 		case 1:
-			if (++matrixCounter > MAX_SIZE) {
+			if (matrixCounter > MAX_SIZE) {
 				printf("There are too many matrices.\n");
 				break;
 			}
-			printf("Type the number of position you want to save matrix : ");
-			scanf("%d", &input);
-			matrices[input] != NULL ? printf("Matrix already exists at the position %d.\n", input) : mread(input);
+			printf("Type the number of position you want to save the matrix : ");
+			scanf("%d", &input1);
+			matrices[input1] != NULL ? printf("Matrix already exists at the position %d.\n", input1) : mread(input1);
 			break;
 		case 2:
 			printf("Type the number of matrix you want to print : ");
-			scanf("%d", &input);
-			matrices[input] == NULL ? printf("There is no matrix at that position.\n") : mwrite(matrices[input]);
+			scanf("%d", &input1);
+			matrices[input1] == NULL ? printf("There is no matrix at that position.\n") : mwrite(matrices[input1]);
 			break;
 		case 3:
 			printf("Type the number of matrix you want to erase : ");
-			scanf("%d", &input);
-			matrices[input] == NULL ? printf("There is no matrix at that position.\n") : merase(&matrices[input]);
+			scanf("%d", &input1);
+			matrices[input1] == NULL ? printf("There is no matrix at that position.\n") : merase(&matrices[input1]);
 			break;
 		case 4:
-			madd();
+			printf("Type the number of two matrices you want to add : ");
+			scanf("%d %d", &input1, &input2);
+			printf("Type the number of position you want to save the result matrix : ");
+			scanf("%d", &input3);
+			madd(matrices[input1], matrices[input2], input3);
 			break;
 		case 5:
-			mmult();
+			printf("Type the number of two matrices you want to multiply : ");
+			scanf("%d %d", &input1, &input2);
+			printf("Type the number of position you want to save the result matrix : ");
+			scanf("%d", &input3);
+			mmult(matrices[input1], matrices[input2], input3);
 			break;
 		case 6:
 			mtranspose();
